00a5e7abdaf89bf843d19bb68312b80ee579225b
choisungwook <kgg1959@naver.com>
2022-09-21 23:57:54 +0900

    라벨 추가

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from prometheus_client import start_http_server, Counter
 
-REQUEST_COUNTER = Counter('app_requests_count', 'total requets count')
+REQUEST_COUNTER = Counter(
+    name='app_requests_count',
+    documentation='total requets count',
+    labelnames=['app_name', 'endpoint']
+)
 
 app = FastAPI()
 
@@ -10,5 +14,5 @@ start_http_server(METRICS_PORT)
 
 @app.get("/")
 def read_root():
-    REQUEST_COUNTER.inc()
+    REQUEST_COUNTER.labels('python-test-app', '/').inc()
     return {"Hello": "World"}
